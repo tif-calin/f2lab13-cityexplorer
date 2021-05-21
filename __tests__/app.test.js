9b47aaf4cc992b062d76b2abbaff1e5ba0def619
tif-calin <culitif@tuta.io>
2021-05-21 15:59:37 -0700

    setup repo

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,31 +12,8 @@ describe('API Routes', () => {
   });
 
   describe('/api/cats', () => {
-    let user;
-
-    beforeAll(async () => {
-      execSync('npm run recreate-tables');
-
-      const response = await request
-        .post('/api/auth/signup')
-        .send({
-          name: 'Me the User',
-          email: 'me@user.com',
-          password: 'password'
-        });
-
-      expect(response.status).toBe(200);
-
-      user = response.body;
-    });
-
-    // append the token to your requests:
-    //  .set('Authorization', user.token);
     
     it('VERB to /api/route [with context]', async () => {
-      
-      // remove this line, here to not have lint error:
-      user.token;
     
       // expect(response.status).toBe(200);
       // expect(response.body).toEqual(?);
