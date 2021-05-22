63dc8ce368d97b225606ee6523f3e49caa718be0
tif-calin <culitif@tuta.io>
2021-05-21 19:35:01 -0700

    implement tests and yelp data

--- __tests__/utils-munge.test.js
+++ __tests__/utils-munge.test.js
@@ -1,21 +1,45 @@
 import locationData from '../data/geo.js';
 import weatherData from '../data/weather.js';
 import yelpData from '../data/yelp.js';
+import { formatLocation, formatWeather, formatYelp } from '../lib/utils-munge.js';
 
-describe('API Routes', () => {
-
-  afterAll(async () => {
-    return client.end();
+describe('munging functions', () => {
+    
+  it('munge /api/location response', async () => {
+    const expected = {
+      formatted_query: 'Portland, Multnomah County, Oregon, USA',
+      latitude: '45.5202471',
+      longitude: '-122.6741949'
+    };
+    const actual = formatLocation(locationData)[0];
+  
+    expect(actual).toEqual(expected);
+    
+  });
+    
+  it('munge /api/weather response', async () => {
+    const expected = {
+      forecast: 'Scattered clouds',
+      time: '2020-05-05'
+    };
+    const actual = formatWeather(weatherData)[0];
+  
+    expect(actual).toEqual(expected);
+    
+  });
+    
+  it('munge /api/reviews response', async () => {
+    const expected = {
+      name: 'Jim’s Good Food',
+      image_url: 'https://s3-media3.fl.yelpcdn.com/bphoto/PvP5Iv3JIuH1TVA1amFsOA/o.jpg',
+      price: '$$',
+      rating: 4.5,
+      url: 'https://www.yelp.com/biz/jim-s-good-food-sacramento?adjust_creative=SIP1Dfwy1M0HWzdgGg9H9g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=SIP1Dfwy1M0HWzdgGg9H9g'
+    };
+    const actual = formatYelp(yelpData)[0];
+  
+    expect(actual).toEqual(expected);
+    
   });
 
-  describe('/api/location', () => {
-    
-    it('GET /api/location', async () => {
-    
-      // expect(response.status).toBe(200);
-      // expect(response.body).toEqual(?);
-      
-    });
-
-  });
 });
--- data/yelp.js
+++ data/yelp.js
@@ -1,5 +1,51 @@
 const yelpData = {
-
+  'businesses': [
+    {
+      'id': '8wVPL9FaYD40mTSa-mmFFw',
+      'alias': 'jim-s-good-food-sacramento',
+      'name': 'Jim’s Good Food',
+      'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/PvP5Iv3JIuH1TVA1amFsOA/o.jpg',
+      'is_closed': true,
+      'url': 'https://www.yelp.com/biz/jim-s-good-food-sacramento?adjust_creative=SIP1Dfwy1M0HWzdgGg9H9g&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=SIP1Dfwy1M0HWzdgGg9H9g',
+      'review_count': 39,
+      'categories': [
+        {
+          'alias': 'newamerican',
+          'title': 'American (New)'
+        }
+      ],
+      'rating': 4.5,
+      'coordinates': {
+        'latitude': 38.572765,
+        'longitude': -121.487812
+      },
+      'transactions': [],
+      'price': '$$',
+      'location': {
+        'address1': '1420 16th St',
+        'address2': '',
+        'address3': null,
+        'city': 'Sacramento',
+        'zip_code': '95814',
+        'country': 'US',
+        'state': 'CA',
+        'display_address': [
+          '1420 16th St',
+          'Sacramento, CA 95814'
+        ]
+      },
+      'phone': '+19169995467',
+      'display_phone': '(916) 999-5467',
+      'distance': 3209.5783705091644
+    }
+  ],
+  'total': 1,
+  'region': {
+    'center': {
+      'longitude': 121.5,
+      'latitude': 38.6
+    }
+  }
 };
 
 export default yelpData;
